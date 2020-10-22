380fc80c8afbd5bc65f434820d71276caa89a028
Dennis231708 <56503655+Dennis231708@users.noreply.github.com>
2020-10-22 17:21:17 -0500

    main files
    main files

---
+++ index.js
@@ -0,0 +1,20 @@
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+
+const perimiter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimiter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart(totalDuration) {
+		duration = totalDuration;
+	},
+	onTick(timeRemaining) {
+		circle.setAttribute('stroke-dashoffset', perimiter * timeRemaining / duration - perimiter);
+	},
+	onComplete() {
+		setInterval(alert('Timer Complete!'), 1000);
+	}
+});
